fe83864d1a8d975a0bd9d4a05d2aeef9ca488080
rayalienkiss <alien1983gs@126.com>
2016-03-28 18:39:07 +0800

    update

--- static/js/framework.js
+++ static/js/framework.js
@@ -1,7 +1,15 @@
-//关闭自动填表
-function noautoform() {
-    var autoform = document.getElementsByTagName("form");
-    for(var i = 0; i < autoform.length; i++)
-        autoform[i].setAttribute("autocomplete", "off");
-};
-noautoform();
+$(function (){
+    //Dom准备就绪加载 BrickPlus Logo 动画样式
+    $('#BrickPlusLogo').addClass('move');
+
+    //BrickPlus 侧导航状态动画
+    $('.bricks-aside-ul-wrap ul li.current').addClass('animation-icon');
+
+    //关闭自动填表
+    function noautoform() {
+        var autoform = document.getElementsByTagName("form");
+        for(var i = 0; i < autoform.length; i++)
+            autoform[i].setAttribute("autocomplete", "off");
+    };
+    noautoform();
+});
