67dd2a1c967eacdeae08c8072e6de2f5e43866c1
tomieric <tomieric@gmail.com>
2016-07-06 14:22:45 +0800

    1. 增加wepbakc-test 2.增加 tab，checkAll 组件

--- src/brickPlus.js
+++ src/brickPlus.js
@@ -3,3 +3,8 @@
  * by tommyshao
  * 2016-07-04
  */
+
+'use strict'
+console.log(123)
+// 全选组件
+let checkAll = require('checkAll')
--- src/checkAll.js
+++ src/checkAll.js
@@ -1,3 +1,18 @@
+/**
+ * @component   : checkAll全选
+ * @version     : {{VERSION}}
+ * @author      : tommyshao
+ * @created     : 2016-07-05
+ * @description :
+ * @useage      :
+ ## 用法
+ ```
+  <input type="checkbox" data-toggle="checkAll" data-target="selector" />
+  $(element).on('checked.ui.checkAll', function(e){ e.relatedTarget; });
+  $(element).on('reversed.ui.checkAll', function(e){ e.relatedTarget; });
+ ```
+ */
+
 'use strict';
 
 const $ = require('jquery')
@@ -10,31 +25,41 @@ class CheckAll{
   // -------
   // * `element` dom元素对象
   constructor(element) {
-    this.state = {};
+    // dom元素
     this.$el = $(element)
+    // 响应元素集合
     this.$target = $(this.$el.data('target'))
-    this.isReverse = this.$el.data('reverse')
+    // 是否反选模式
+    this.isReverse = Boolean(this.$el.data('reverse'))
 
+    // 版本号
     this.VERSION = '{{VERSION}}';
 
+    // 初始化事件
     this.initEvents()
   }
 
   // 事件监听
   initEvents() {
     // 监听 `click` 点击事件
-    this.$el.on('click', $.proxy(this.isReverse ? this.reverse : this.activate, this))
+    this.$el.on('click', $.proxy(this.toggle, this))
+  }
+
+  // 切换中枢
+  // -------
+  toggle () {
+    this.isReverse ? this.reverse() : this.activate()
   }
 
   // 全选功能
   // --------
   // Function activate
-  activate() {
+  activate(isChecked) {
     let [
       isCheck,
       e
     ] = [
-      this.$el.is(':checked'), // 当前dom元素是否勾选
+      isChecked || this.$el.is(':checked'), // 当前dom元素是否勾选
       $.Event('checked.ui.checkAll', { relatedTarget: this.$el }) // 创建选中事件
     ]
 
@@ -51,7 +76,9 @@ class CheckAll{
     let e = $.Event('reversed.ui.checkAll', {relatedTarget: this.$el})
     // 遍历所有目标元素，将他们选中属性反转
     this.$target.map(function () {
-        return $(this).prop('checked', !this.checked)
+        return $(this).prop('checked', function() {
+          return !$(this).prop('checked')
+        })
     });
     // 触发反选事件api
     this.$el.trigger(e);
@@ -60,15 +87,19 @@ class CheckAll{
 
 // 插件定义
 // -------
-let Plugin = function(option) {
+let Plugin = function(option, ...args) {
   return $(this).each(()=> {
     let [$this, data] = [
       $(this),
       $(this).data('ui.checkAll')
     ];
 
-    if (!data) $this.data('ui.checkAll', (data = new CheckAll(this)));
-    if (typeof option == 'string') data[option]();
+    if (!data) {
+      $this.data('ui.checkAll', (data = new CheckAll(this)));
+      if (option === 'toggle') data.toggle();
+    }
+
+    if (typeof option === 'string' && option !== 'toggle') data[option](...args);
   })
 }
 
@@ -80,6 +111,14 @@ $.fn.checkAll.Constructor = CheckAll;
 
 // 全局绑定插件
 // -------------
-$(function () {
-    $(toggle).checkAll()
-});
+// $(function () {
+//     $(toggle).checkAll()
+
+// });
+$(function() {
+  $(document).on('click.checkAll', toggle, function() {
+    $(this).checkAll('toggle')
+  })
+})
+
+module.exports = CheckAll
