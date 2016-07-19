3354a1a2fa30e3390dc2ac63d897c7fc5083498c
tomieric <tomieric@gmail.com>
2016-07-19 17:55:07 +0800

    tooltips组件

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = function(entries, output) {
     //context: path.resolve(__dirname),
     //entry: entryFiles,
     entry: {
-      alert: './src/alert.js'
+      tooltips: './src/tooltips.js'
     },
     output: {
       path: path.join(__dirname, './static/js/brickplus'),
