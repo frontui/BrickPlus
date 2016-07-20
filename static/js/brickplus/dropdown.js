41b80455f39082ca10bb3e1416b07d0fe69b5181
tomieric <tomieric@gmail.com>
2016-07-20 09:26:35 +0800

    Update 2016-07-20T01:26:29.857Z

--- static/js/brickplus/dropdown.js
+++ static/js/brickplus/dropdown.js
@@ -1,7 +1,7 @@
 /*! generator-frontman v0.1.2
 *  by fronui team
 *  (c) 2014-2016 www.frontpay.cn
-* updated on 2016-07-19
+* updated on 2016-07-20
 *  Licensed under MIT
 */
  !function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.BrickPlus=t():e.BrickPlus=t()}(this,function(){return function(e){function t(r){if(o[r])return o[r].exports;var n=o[r]={exports:{},id:r,loaded:!1};return e[r].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var o={};return t.m=e,t.c=o,t.p="",t(0)}([function(e,t){}])});
