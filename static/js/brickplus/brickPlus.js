4f76353b4d7345971f980ae4ddc7e3b731aaba41
tomieric <tomieric@gmail.com>
2016-07-23 11:38:37 +0800

    Update 2016-07-23T03:38:01.498Z

---
+++ static/js/brickplus/brickPlus.js
@@ -0,0 +1,7 @@
+/*! generator-frontman v0.1.2
+*  by fronui team
+*  (c) 2014-2016 www.frontpay.cn
+* updated on 2016-07-23
+*  Licensed under MIT
+*/
+ !function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o(require("jquery")):"function"==typeof define&&define.amd?define(["jquery"],o):"object"==typeof exports?exports.BrickPlus=o(require("jquery")):e.BrickPlus=o(e.jQuery)}(this,function(e){return function(e){function o(t){if(r[t])return r[t].exports;var n=r[t]={exports:{},id:t,loaded:!1};return e[t].call(n.exports,n,n.exports,o),n.loaded=!0,n.exports}var r={};return o.m=e,o.c=r,o.p="",o(0)}([function(e,o,r){"use strict";var t=r(1),n={};r(!function(){var e=new Error('Cannot find module "./checkAll"');throw e.code="MODULE_NOT_FOUND",e}()),r(!function(){var e=new Error('Cannot find module "./tab"');throw e.code="MODULE_NOT_FOUND",e}());n.Util=r(!function(){var e=new Error('Cannot find module "./brickPlus.Util"');throw e.code="MODULE_NOT_FOUND",e}()),t(function(){t("form").attr("autocomplete","off")}),t.brickPlus=n,e.exports=n},function(o,r){o.exports=e}])});
