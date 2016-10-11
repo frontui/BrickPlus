508f59622e007a1885afecf56402804df3636c65
limit <minyi.lin@frontpay.cn>
2016-10-11 14:36:23 +0800

    update:大账户那边联调switch二次确认出BUG，再更新一个版本

--- src/switch.js
+++ src/switch.js
@@ -19,6 +19,8 @@ export default class Switch {
         //  是否点击时需要二次确认
         this.isSecondCheck = Boolean(this.el.data('issecondcheck'));
 
+        this.isChecked = this.el.hasClass('checked');
+
         if(!props.docClick) this.el.on('click', $.proxy(this.toggle, this)) 
     }
 
@@ -27,26 +29,28 @@ export default class Switch {
         // 禁止状态
         if(this.el.hasClass('disabled')) return;
 
-        var checked = this.el.hasClass('checked')
+        // var checked = this.el.hasClass('checked')
 
         if(this.isSecondCheck){
             //  弹窗二次确认
-            var status = checked ? '激活' : '未激活';
-            var toStatus = checked ? '取消激活' : '激活';
+            var status = this.isChecked ? '激活' : '未激活';
+            var toStatus = this.isChecked ? '取消激活' : '激活';
             var content = '当前状态为' + status + '，是否' + toStatus + '?';
             $.confirmModalLayer({ 
                 title: '提醒', 
                 content: content, 
-                callback: this._toggleClass(checked)
+                callback: this._toggleClass.bind(this)
             });
         }else{
-            this._toggleClass(checked);
+            this._toggleClass();
         }
 
     }
 
-    _toggleClass(checked){
-        this.el.toggleClass('checked', !checked).trigger('checked.bp.switch', !checked)
+    _toggleClass(){
+        this.el.toggleClass('checked', !this.isChecked).trigger('checked.bp.switch', !this.isChecked)
+        //  控件状态同步
+        this.isChecked = !this.isChecked;
     }
 }
 
