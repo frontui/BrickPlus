dfb55a25ac8158b3a56af751485bd9f675158fa4
yyhuaisha <2433273227@qq.com>
2016-11-22 10:17:10 +0800

    Update 2016-11-22T02:15:37.276Z

--- static/js/brickplus/placeholder.js
+++ static/js/brickplus/placeholder.js
@@ -1,7 +1,7 @@
 /*! 
-*  BrickPlus v1.0.10
+*  BrickPlus v1.1.3
 *  by fronui team
-*  updated on 2016-10-12
+*  updated on 2016-11-22
 *  created by generator-frontman
 *  (c) 2014-2016 www.frontpay.cn
 *  Licensed under MIT
