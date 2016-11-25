12a8528a9054862817b8cc355d89d2ea89a9e363
yyhuaisha <2433273227@qq.com>
2016-11-25 16:17:05 +0800

    v1.1.7 发版

--- dist/js/cookie.js
+++ dist/js/cookie.js
@@ -1,7 +1,7 @@
 /*! 
-*  BrickPlus v1.1.5
+*  BrickPlus v1.1.7
 *  by fronui team
-*  updated on 2016-11-23
+*  updated on 2016-11-25
 *  created by generator-frontman
 *  (c) 2014-2016 www.frontpay.cn
 *  Licensed under MIT
